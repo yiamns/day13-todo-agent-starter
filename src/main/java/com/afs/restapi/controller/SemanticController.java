b19fbb0ad0019ec33410853c00152dab8f5a728e
Phodal Huang <h@phodal.com>
2025-09-16 22:18:34 +0800

    fix: correct typo in system message for Todo tasks manager

--- src/main/java/com/afs/restapi/controller/SemanticController.java
+++ src/main/java/com/afs/restapi/controller/SemanticController.java
@@ -43,7 +43,7 @@ public class SemanticController {
     @GetMapping(value = "/chat/todo", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     Flux<String> simplePromptExample(@RequestParam String userInput) {
         return this.chatClient.prompt()
-                .system("Your are an Todo tasks manager, you should only answer about Todo manage related questions." +
+                .system("You are a Todo tasks manager, you should only answer about Todo manage related questions." +
                         "If the user ask you about other things, you should answer you don't know. ")
                 .user(userInput)
                 .stream()
